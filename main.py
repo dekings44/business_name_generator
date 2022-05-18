d63012f1c4daa6a40b9c3cdedecfb0c937645b70
dekings44 <dekings44@gmail.com>
2022-05-18 23:32:30 +0100

    added the length of business name

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ favourite_colour = input('What is your favourite colour?\n')
 
 business_name = first_name[:3] + favourite_colour[-4:]
 
-print('Your business name is {} Enterprise'.format(business_name))
+bus_name_len = len(business_name)
+
+print('Your business name is {} Enterprise and it is made of {} letters'.format(business_name, bus_name_len))
